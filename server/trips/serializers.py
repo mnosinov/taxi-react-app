5686d0a822721256876ff8533ce1d20ba17d7f37
mnosinov <mnosinov@gmail.com>
2021-08-04 13:51:41 +0600

    intermediate commit.

--- server/trips/serializers.py
+++ server/trips/serializers.py
@@ -45,3 +45,13 @@ class TripSerializer(serializers.ModelSerializer):
         model = Trip
         fields = '__all__'
         read_only_fields = ('id', 'created', 'updated',)
+
+
+class NestedTripSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trip
+        fields = '__all__'
+        depth = 1
+
+    driver = UserSerializer()
+    rider = UserSerializer()
