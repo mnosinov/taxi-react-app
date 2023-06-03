865b466376e75f32c51ed117340e117b4582c5c3
mnosinov <mnosinov@gmail.com>
2023-06-03 13:51:34 +0600

    create trip action is ready to use

--- server/trips/models.py
+++ server/trips/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.shortcuts import reverse
+from django.conf import settings
 
 
 class User(AbstractUser):
@@ -28,6 +29,20 @@ class Trip(models.Model):
     drop_off_address = models.CharField(max_length=255)
     status = models.CharField(
         max_length=20, choices=STATUSES, default=REQUESTED
+    )
+    driver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.DO_NOTHING,
+        related_name='trips_as_driver'
+    )
+    rider = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.DO_NOTHING,
+        related_name='trips_as_rider'
     )
 
     def __str__(self):
