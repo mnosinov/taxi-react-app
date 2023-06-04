976760a1ee41fcd97c751b783c5ba4ec3c6005df
mnosinov <mnosinov@gmail.com>
2023-06-04 13:58:16 +0600

    server part is ready to use

--- server/trips/tests/test_http.py
+++ server/trips/tests/test_http.py
@@ -1,6 +1,10 @@
 import base64
 import json
 
+from io import BytesIO
+from PIL import Image
+
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from rest_framework import status
@@ -20,9 +24,17 @@ def create_user(username='user@example.com', password=PASSWORD, group_name='ride
     user.save()
     return user
 
+def create_photo_file():
+    data = BytesIO()
+    Image.new('RGB', (100, 100)).save(data, 'PNG')
+    data.seek(0)
+    return SimpleUploadedFile('photo.png', data.getvalue())
+
+
 
 class AuthenticationTest(APITestCase):
     def test_user_can_sign_up(self):
+        photo_file = create_photo_file()
         response = self.client.post(reverse('sign_up'), data={
             'username': 'user@example.com',
             'first_name': 'Test',
@@ -30,8 +42,8 @@ class AuthenticationTest(APITestCase):
             'password1': PASSWORD,
             'password2': PASSWORD,
             'group': 'rider',
+            'photo': photo_file,
         })
-
         user = get_user_model().objects.last()
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(response.data['id'], user.id)
@@ -39,6 +51,7 @@ class AuthenticationTest(APITestCase):
         self.assertEqual(response.data['first_name'], user.first_name)
         self.assertEqual(response.data['last_name'], user.last_name)
         self.assertEqual(response.data['group'], user.group)
+        self.assertIsNotNone(user.photo)
 
     def test_user_can_log_in(self):
         user = create_user()
