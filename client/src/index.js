f839623c09b8baddeba613e8e7aa2adc973b77be
mnosinov <mnosinov@gmail.com>
2023-06-05 14:22:32 +0600

    routing had been added

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Landing from './components/Landing.js';
+import LogIn from './components/LogIn.js';
+import SignUp from './components/SignUp.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+		<HashRouter>
+			<Routes>
+				<Route path='/' element={<App/>} />
+				<Route index element={<Landing/>} />
+				<Route path='sign-up' element={<SignUp/>} />
+				<Route path='log-in' element={<LogIn/>} />
+			</Routes>
+		</HashRouter>
   </React.StrictMode>
 );
 
