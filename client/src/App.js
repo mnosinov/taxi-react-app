e7f3fbb3330654e45ebb768d9d520aaa4f08c77d
mnosinov <mnosinov@gmail.com>
2023-06-07 12:15:06 +0600

    logout button prepared

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import { Container, Navbar }  from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Navbar, Button, Form }  from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Outlet, Route, Routes } from 'react-router-dom';
+
 import Landing from './components/Landing.js';
 import LogIn from './components/LogIn.js';
 import SignUp from './components/SignUp.js';
+
 import './App.css';
 
 function App() {
+	const [isLoggedIn, setLoggedIn] = useState(false);
+	const logIn = (username, passwrod) => setLoggedIn(true);
+
 	return (
 		<Routes>
-			<Route path='/' element={<Layout/>}>
+			<Route path='/' element={<Layout isLoggedIn={isLoggedIn} />}>
 				<Route index element={<Landing/>} />
 				<Route path='sign-up' element={<SignUp/>} />
-				<Route path='log-in' element={<LogIn/>} />
+				<Route path='log-in' element={<LogIn logIn={logIn} />} />
 			</Route>
 		</Routes>
 	);
 }
 
-function Layout () {
+function Layout ({ isLoggedIn }) {
   return (
 		<>
 			<Navbar bg='light' expand='lg' variant='light'>
@@ -28,7 +33,15 @@ function Layout () {
 						<Navbar.Brand className='logo'>Taxi</Navbar.Brand>
 					</LinkContainer>
 					<Navbar.Toggle />
-					<Navbar.Collapse />
+					<Navbar.Collapse className='justify-content-end'>
+						{
+							isLoggedIn && (
+								<Form>
+									<Button type='button'>Log out</Button>
+								</Form>
+							)
+						}
+					</Navbar.Collapse>
 				</Container>
 			</Navbar>
 			<Container className='pt-3'>
